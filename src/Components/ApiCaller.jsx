5425c1f0f682a24b481dff96104acb2a84593e8e
Sahilakodia09 <141423813+Sahilakodia09@users.noreply.github.com>
2023-08-28 14:52:57 +0530

    Update ApiCaller.jsx

--- src/Components/ApiCaller.jsx
+++ src/Components/ApiCaller.jsx
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const headerMap = {
-    authorization: '',
-    'Content-Type': 'application/json'
-};
+
 
 export const callPostApi = (url, data, successCallBack, failedCallBack) => {
     axios.post(url, data).then((result) => {
