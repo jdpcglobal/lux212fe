9b608e8394452773afbbb0c31cb79f56b64c206f
Sanjay Choudhary <sanjaychoudhary5501@gmail.com>
2024-11-07 16:10:43 +0530

    Revert "new"
    This reverts commit da606682c4ac2731bdc34e8184c7236d8c3686da.

--- src/Components/SidebarComponents/BankTransaction.jsx
+++ src/Components/SidebarComponents/BankTransaction.jsx
@@ -1,53 +1,48 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Table } from 'reactstrap';
 import Cookies from 'universal-cookie';
 import { callPostApi } from '../ApiCaller';
 import { BankTransactions_Post } from '../ApiConst';
 
 const BankTransaction = () => {
+  const [transactionData, setTransactionData] = useState([]);
+  const token = new Cookies().get("kisDiamond_LoggedIn")?.Token;
 
-    const [transactionData, setTransactionData] = useState([]);
-    const token = new Cookies().get("kisDiamond_LoggedIn")?.Token;
+  useEffect(() => {
+    fetchBankTransactions();
+  }, []);
 
-    useEffect(() => {
-        fetchBankTransactions();
-    }, []);
+  const fetchBankTransactions = async () => {
+    const formData = new FormData();
+    formData.append('Token', token);
+    callPostApi(BankTransactions_Post, formData, jsonData => {
+      const respObj = jsonData.data;
+      toast(respObj.message, {
+        type: jsonData.isSuccess ? 'success' : 'error',
+      });
+      if (respObj.isSuccess) {
+        setTransactionData(respObj.data);
+      }
+    });
+  };
 
-    const fetchBankTransactions = async () => {
-        const formData = new FormData();
-        formData.append('Token', token);
-        callPostApi(BankTransactions_Post, formData, jsonData => {
-            const respObj = jsonData.data;
-            toast(respObj.message, {
-                type: jsonData.isSuccess ? 'success' : 'error',
-            });
-            if (respObj.isSuccess) {
-                setTransactionData(respObj.data);
-            }
-        })
-
-    };
-
-    return (
-        <>
-            <div id="BankTransaction" style={{ width: '100%' }} className="offcanvas offcanvas-end bg-theme">
-            <div className="title-section">
-          <span>Deposit</span>
-          <div className="align-self-center ms-auto">
-                                <a href="#" data-bs-dismiss="offcanvas" className="icon icon-m"><i className="bi bi-x-circle-fill color-red-dark font-18 me-n4" /></a>
-                            </div>
+  return (
+    <>
+      <div id="BankTransaction">
+        <div className="title-section">
+          <span>Bank Transaction</span>
         </div>
         {transactionData.map((transaction, index) => (
           <div key={index} className="transaction-table">
             <div className='border-row'>
-            <div className="transaction-row">
-            <div className="label-deposit">Deposit</div>
-            <div className="value" style={{ color: transaction.Status === "Pending" ? "red" : "green" }}>
-                        {transaction.Status === "Pending" ? "PENDING" : "APPROVED"}
-                      </div>
-              </div>
+            <div className="transaction-row ">
+                
+              <div className="label">Deposit:</div>
+              <div className="value" style={{ color: transaction.Status === "Pending" ? "red" : "green" }}>
+  {transaction.Status === "Pending" ? "PENDING" : "APPROVED"}
+</div>
+</div>
             </div>
             <div className="transaction-row">
               <div className="label">Balance:</div>
@@ -68,9 +63,9 @@ const BankTransaction = () => {
           </div>
         ))}
       </div>
-            
-        </>
-    )
-}
+      <ToastContainer />
+    </>
+  );
+};
 
-export default BankTransaction
+export default BankTransaction;
